c0e9d6bad806abeac6b319def07947522793c00b
anas-alnabahin <anasnb2002nb@gmail.com>
2025-01-03 15:17:22 +0200

    initial style for auth page.... still needs work

--- src/routes/authentication/authentication.component.tsx
+++ src/routes/authentication/authentication.component.tsx
@@ -1,6 +1,18 @@
-import "./authentication.styles.css";
+import Button from "../../components/button/button.component";
+import FormInput from "../../components/form-input/form-input.component";
+
+import { AuthContainer, AuthFormsContainer } from "./authentication.styles";
 
 const Authentication = () => {
-  return <div>Authentication</div>;
+  return (
+    <AuthContainer>
+      <AuthFormsContainer>
+        <span>Sign in to the Dashboard</span>
+        <FormInput label="email" />
+        <FormInput label="password" />
+        <Button>Sign In</Button>
+      </AuthFormsContainer>
+    </AuthContainer>
+  );
 };
 export default Authentication;
---
+++ src/routes/authentication/authentication.styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import {
+  CenteredColFlexContainer,
+  CenteredFlexContainer,
+  SecondaryColors,
+} from "../../styles/general.styles";
+
+export const AuthContainer = styled.div`
+  ${CenteredFlexContainer}
+  height: 100vh;
+  background-image: url("/images/gym-bg.jpg");
+`;
+
+export const AuthFormsContainer = styled.div`
+  ${CenteredColFlexContainer}
+  ${SecondaryColors}
+  gap: 1em;
+  border-radius: 26px;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+  min-width: 550px;
+  height: 50%;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    width: ${({ theme }) => theme.grid.col5};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    width: 100%;
+    border-radius: 0;
+  }
+`;
